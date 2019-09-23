55b4859ba062c7201974a121d2a6dc958637a6b1
Raul Metsma <raul@metsma.ee>
2019-09-23 15:21:15 +0300

    OpenSC 0.20.0.2 build

---
+++ OpenSC-sign.sh
@@ -0,0 +1,5 @@
+pkgutil --expand-full $1 opensc_pkg
+codesign -f --options=runtime -s "Developer ID Application: Riigi" opensc_pkg/Payload/Library/OpenSC/lib/*.*
+codesign -f --entitlements OpenSC.entitlements --options=runtime -s "Developer ID Application: Riigi" opensc_pkg/Payload/Library/OpenSC/bin/* 
+pkgutil --flatten-full opensc_pkg $1
+rm -rf opensc_pkg
